e423b81791fbbff1636f74ef53b482ca9a084a70
vito cervelli <vcervelli@navicu.com>
2019-09-03 13:44:27 -0400

    Agrega entidades y relaciones

---
+++ src/Entity/Rol.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\RolRepository")
+ */
+class Rol
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\User", mappedBy="rol")
+     */
+    private $users;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $date_created;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $date_update;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $first_user_modification;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $last_user_modification;
+
+
+    public function __construct()
+    {
+        $this->roles = new ArrayCollection();
+        $this->users = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|User[]
+     */
+    public function getUsers(): Collection
+    {
+        return $this->users;
+    }
+
+    public function addUser(User $user): self
+    {
+        if (!$this->users->contains($user)) {
+            $this->users[] = $user;
+            $user->setRol($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUser(User $user): self
+    {
+        if ($this->users->contains($user)) {
+            $this->users->removeElement($user);
+            // set the owning side to null (unless already changed)
+            if ($user->getRol() === $this) {
+                $user->setRol(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getDateCreated(): ?\DateTimeInterface
+    {
+        return $this->date_created;
+    }
+
+    public function setDateCreated(?\DateTimeInterface $date_created): self
+    {
+        $this->date_created = $date_created;
+
+        return $this;
+    }
+
+    public function getDateUpdate(): ?\DateTimeInterface
+    {
+        return $this->date_update;
+    }
+
+    public function setDateUpdate(?\DateTimeInterface $date_update): self
+    {
+        $this->date_update = $date_update;
+
+        return $this;
+    }
+
+    public function getFirstUserModification(): ?\DateTimeInterface
+    {
+        return $this->first_user_modification;
+    }
+
+    public function setFirstUserModification(?\DateTimeInterface $first_user_modification): self
+    {
+        $this->first_user_modification = $first_user_modification;
+
+        return $this;
+    }
+
+    public function getLastUserModification(): ?\DateTimeInterface
+    {
+        return $this->last_user_modification;
+    }
+
+    public function setLastUserModification(?\DateTimeInterface $last_user_modification): self
+    {
+        $this->last_user_modification = $last_user_modification;
+
+        return $this;
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Rol", inversedBy="users")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $rol;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $age;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $date_created;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $date_update;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $first_user_modification;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $last_user_modification;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRol(): ?Rol
+    {
+        return $this->rol;
+    }
+
+    public function setRol(?Rol $rol): self
+    {
+        $this->rol = $rol;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAge(): ?int
+    {
+        return $this->age;
+    }
+
+    public function setAge(?int $age): self
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    public function getDateCreated(): ?\DateTimeInterface
+    {
+        return $this->date_created;
+    }
+
+    public function setDateCreated(?\DateTimeInterface $date_created): self
+    {
+        $this->date_created = $date_created;
+
+        return $this;
+    }
+
+    public function getDateUpdate(): ?\DateTimeInterface
+    {
+        return $this->date_update;
+    }
+
+    public function setDateUpdate(?\DateTimeInterface $date_update): self
+    {
+        $this->date_update = $date_update;
+
+        return $this;
+    }
+
+    public function getFirstUserModification(): ?\DateTimeInterface
+    {
+        return $this->first_user_modification;
+    }
+
+    public function setFirstUserModification(?\DateTimeInterface $first_user_modification): self
+    {
+        $this->first_user_modification = $first_user_modification;
+
+        return $this;
+    }
+
+    public function getLastUserModification(): ?\DateTimeInterface
+    {
+        return $this->last_user_modification;
+    }
+
+    public function setLastUserModification(?\DateTimeInterface $last_user_modification): self
+    {
+        $this->last_user_modification = $last_user_modification;
+
+        return $this;
+    }
+}
