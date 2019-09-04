c277b7973655139f34de149900946f75f2aafb96
Jorge <jrojas@jacidi.com>
2019-09-04 11:38:46 -0400

    agregado controlador y handler de usuarios

---
+++ src/Controller/User/UserController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Controller\User;
+
+
+use App\Vicander\Handler\User\ListUserHandler;
+use App\Vicander\Handler\User\DetailUserHandler;
+use App\Vicander\Handler\User\CreateUserHandler;
+use App\Vicander\Handler\User\UpdateUserHandler;
+use App\Vicander\Handler\User\DeleteUserHandler;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+
+/**
+ * @Route("/users")
+ */
+class UserController extends AbstractController
+{
+
+    /**
+     * Listar Usuarios
+     *
+     * @Route("", name="list_users", methods="GET")
+     *
+     * @return JsonResponse
+     */
+    public function listUser(Request $request)
+    {
+
+        $handler = new ListUserHandler($request);
+        $handler->processHandler();
+
+        return  $handler->getJsonResponseData();
+    }
+
+
+    /**
+     * Crear Usuarios
+     *
+     * @Route("", name="create_users", methods="POST")
+     *
+     * @return JsonResponse
+     */
+     public function createUser(Request $request)
+    {
+
+        $handler = new CreateUserHandler($request);
+        $handler->processHandler();
+
+        return  $handler->getJsonResponseData();
+    }
+
+    /**
+     * Obtener Usuarios
+     *
+     * @Route("/{id}", name="detail_users", methods="GET")
+     *
+     * @return JsonResponse
+     */
+     public function detailUser(Request $request)
+    {
+
+        $handler = new DetailUserHandler($request);
+        $handler->processHandler();
+
+        return  $handler->getJsonResponseData();
+    }
+
+    /**
+     * Actualizar Usuarios
+     *
+     * @Route("/{id}", name="update_users", methods="PUT")
+     *
+     * @return JsonResponse
+     */
+     public function updateUser(Request $request)
+    {
+
+        $handler = new UpdateUserHandler($request);
+        $handler->processHandler();
+
+        return  $handler->getJsonResponseData();
+    }
+
+    /**
+     * Eliminar Usuarios
+     *
+     * @Route("/{id}", name="delete_users", methods="DELETE")
+     *
+     * @return JsonResponse
+     */
+     public function deleteUser(Request $request)
+    {
+
+        $handler = new DeleteUserHandler($request);
+        $handler->processHandler();
+
+        return  $handler->getJsonResponseData();
+    }
+
+
+
+}
---
+++ src/Vicander/Handler/User/CreateUserHandler.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Vicander\Handler\User;
+
+use App\Vicander\Handler\BaseHandler;
+use App\Entity\User;
+use App\Vicander\Exception\VicanderException;
+
+/**
+ * Prueba de la api
+ *
+ */
+class CreateUserHandler extends BaseHandler
+{
+
+    /**
+     * Aqui va la logica
+     *
+     * @return array
+     */
+    protected function handler() : array
+    {
+        $params = $this->getParams();
+        $manager = $this->getDoctrine()->getManager();
+
+        $users = $manager->getRepository(User::class)->findAll();
+
+        $response = [];
+
+        foreach ($users as $key => $value) {
+        	$response[] = [
+        		'id' => $value->getId(),
+        		'name' => $value->getName(),
+        		'roll' => $value->getRoll(),
+        		'age' => $value->getAge()
+        	];
+        }
+        return $respose;
+    }
+
+    /**
+     * Todas las reglas de validacion para los parametros que recibe
+     * el Handler
+     *
+     * Las reglas de validacion estan definidas en:
+     * @see \App\Navicu\Service\NavicuValidator
+     *
+     * @return array
+     */
+    protected function validationRules() : array
+    {
+        return [
+        ];
+    }
+}
